0f3dc6b32413fcfc1e00d6beb46b8670322a7f7a
Hansal Shah <hansalsh@usc.edu>
2015-03-10 20:22:06 -0700

    proc.c updated do_waitpid in comment

--- kernel/proc/proc.c
+++ kernel/proc/proc.c
@@ -288,7 +288,6 @@ proc_kill(proc_t *p, int status)
 	if(curproc == p) {
 		dbg(DBG_PRINT, "(GRADING1C 9)\n");
 		dbg(DBG_PRINT, "INFO : proc_kill is called on the curproc\n");
-		/*do_exit(status);*/
 		kthread_cancel(curthr, (void*)status);
 		return;
 	}
